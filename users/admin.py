acd27253892297e4d769e496a13bb7d241216415
René Fleschenberg <rene@fleschenberg.net>
2020-04-17 13:48:23 +0200

    Bugfix: Missing import for base class

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     search_fields = ['email', ]
     list_display = ('email', 'is_staff', 'is_superuser', 'created_at')
     ordering = ('-created_at',)
